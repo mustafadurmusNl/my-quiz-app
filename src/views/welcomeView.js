5d015e531281d69a4098fa06f051e2759f4c9c88
mahtabmardani88 <mahtab.mardani88@gmail.com>
2024-05-27 22:53:50 +0200

    add inpput and btn for confirm the player name and can whith start quiz btn start the game almost added and updated

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -5,10 +5,14 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  * @returns {Element}
  */
 export const createWelcomeElement = () => {
+  // create p tag and input field and confirm button 
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
+  <h1 id="welcome-message">Welcome</h1>
+  <p id="name-prompt">What name do you like to play?</p>
+  <input type="text" id="player-name-input" placeholder="Enter your name" />
+  <button id="confirm-button" style="display: block;">Confirm</button>
+  <button id="${START_QUIZ_BUTTON_ID}" style="display: none;">Start quiz</button>
   `;
   return element;
 };
