5b913fe7ba54748ea71deca98b6be9da06c815e1
Mustafa Durmus <mdurmusnl@gmail.com>
2024-05-27 22:20:44 +0200

    title&favicon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+
+
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
+
+
+
 const loadApp = () => {
+
   quizData.currentQuestionIndex = 0;
 
   initWelcomePage();
