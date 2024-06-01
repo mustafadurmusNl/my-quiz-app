a773a841d3f005836791b757867160071c6a74f4
Ali <ali.ibrahim.work@gmail.com>
2024-06-01 18:20:48 +0200

    style.css
    style.css

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -2,9 +2,18 @@
  * Create an Answer element
  * @returns {Element}
  */
+
 export const createAnswerElement = (key, answerText) => {
-  const li = document.createElement('li');
-  li.setAttribute('data-key', key); // Set the data-key attribute to the key parameter
-  li.textContent = answerText;
-  return li;
+  const element = document.createElement('li');
+  element.innerHTML = String.raw`
+    ${key.toUpperCase()}: ${answerText.toUpperCase()}
+  `;
+  return element;
 };
+
+// export const createAnswerElement = (key, answerText) => {
+//   const li = document.createElement('li');
+//   li.setAttribute('data-key', key); // Set the data-key attribute to the key parameter
+//   li.textContent = answerText;
+//   return li;
+// };
