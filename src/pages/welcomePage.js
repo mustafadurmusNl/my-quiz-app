2eb0702ee6c3ebf0cc4ed16b802061a498597b8f
Mustafa Sarıtaş <mstfsrts@gmail.com>
2024-06-01 14:01:55 +0200

    localStorage implementation Added

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,4 +1,12 @@
-import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID , CONFIRM_BUTTON_ID ,INPUT_NAME_ID , INPUT_P_TAG_ID , WELCOME_MESSAGE_ID  } from '../constants.js';
+import {
+  USER_INTERFACE_ID,
+  START_QUIZ_BUTTON_ID,
+  CONFIRM_BUTTON_ID,
+  INPUT_NAME_ID,
+  INPUT_P_TAG_ID,
+  WELCOME_MESSAGE_ID,
+  CONTINUE_BUTTON_ID,
+} from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 
@@ -12,37 +20,68 @@ export const initWelcomePage = () => {
   document
     .getElementById(START_QUIZ_BUTTON_ID)
     .addEventListener('click', startQuiz);
-    const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
-    const startButton = document.getElementById(START_QUIZ_BUTTON_ID);
-    const nameInput = document.getElementById(INPUT_NAME_ID);
-    const namePrompt = document.getElementById(INPUT_P_TAG_ID);
-    const welcomeMessage = document.getElementById(WELCOME_MESSAGE_ID);
-  
- 
-    function updateButtonState() {
-      nameInput.value = nameInput.value.trimStart(); 
-      const name = nameInput.value;
-      if (name) {
-        confirmButton.classList.remove('button-disabled');
-        confirmButton.classList.add('button-enabled');
-        confirmButton.disabled = false;
-        confirmButton.style.backgroundColor = 'rgb(221, 113, 162)';
-        confirmButton.style.cursor = 'pointer';
-      } else {
-        confirmButton.classList.add('button-disabled');
-        confirmButton.classList.remove('button-enabled');
-        confirmButton.disabled = true;
-        confirmButton.style.backgroundColor = 'rgb(232, 232, 232)'; 
-        confirmButton.style.cursor = 'not-allowed';
-      }
-    }
-  
-    updateButtonState();
-    nameInput.addEventListener('input', updateButtonState);
+  const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
+  const continueButton = document.getElementById(CONTINUE_BUTTON_ID);
+  const startButton = document.getElementById(START_QUIZ_BUTTON_ID);
+  const nameInput = document.getElementById(INPUT_NAME_ID);
+  const namePrompt = document.getElementById(INPUT_P_TAG_ID);
+  const welcomeMessage = document.getElementById(WELCOME_MESSAGE_ID);
 
-    confirmButton.addEventListener('click', () => {
-      const playerName = nameInput.value.trim();
-      if (playerName) {
+  function updateConfirmButtonState() {
+    nameInput.value = nameInput.value.trimStart();
+    const name = nameInput.value;
+    if (name) {
+      confirmButton.classList.remove('button-disabled');
+      confirmButton.classList.add('button-enabled');
+      confirmButton.disabled = false;
+      confirmButton.style.backgroundColor = 'rgb(221, 113, 162)';
+      confirmButton.style.cursor = 'pointer';
+    } else {
+      confirmButton.classList.add('button-disabled');
+      confirmButton.classList.remove('button-enabled');
+      confirmButton.disabled = true;
+      confirmButton.style.backgroundColor = 'rgb(232, 232, 232)';
+      confirmButton.style.cursor = 'not-allowed';
+    }
+  }
+
+  function updateContinueButtonState() {
+    const storagedPlayerName = localStorage.getItem('playerName');
+    if (storagedPlayerName) {
+      continueButton.classList.remove('button-disabled');
+      continueButton.classList.add('button-enabled');
+      continueButton.disabled = false;
+      continueButton.style.backgroundColor = 'rgb(221, 113, 162)';
+      continueButton.style.cursor = 'pointer';
+    } else {
+      continueButton.classList.add('button-disabled');
+      continueButton.classList.remove('button-enabled');
+      continueButton.disabled = true;
+      continueButton.style.backgroundColor = 'rgb(232, 232, 232)';
+      continueButton.style.cursor = 'not-allowed';
+    }
+  }
+
+  updateConfirmButtonState();
+  nameInput.addEventListener('input', updateConfirmButtonState);
+
+  updateContinueButtonState();
+
+  confirmButton.addEventListener('click', () => {
+    const playerName = nameInput.value.trim();
+    const storagedPlayerName = localStorage.getItem('playerName');
+    if (playerName) {
+      if (playerName === storagedPlayerName) {
+        const storagedQuestionIndex = localStorage.getItem('questionIndex');
+        console.log('storagedQuestionIndex', storagedQuestionIndex);
+        if (storagedQuestionIndex) {
+          initQuestionPage(Number(storagedQuestionIndex));
+        } else {
+          initQuestionPage();
+        }
+      } else {
+        localStorage.setItem('score', 0);
+        localStorage.setItem('playerName', playerName);
         welcomeMessage.innerText = `Welcome , ${playerName}`;
         welcomeMessage.classList.add('welcome-effect');
         welcomeMessage.style.padding = '20px';
@@ -51,18 +90,28 @@ export const initWelcomePage = () => {
           welcomeMessage.classList.add('green-text');
         }, 100);
         confirmButton.style.display = 'none';
+        continueButton.style.display = 'none';
         startButton.style.display = 'block';
         nameInput.style.display = 'none';
         namePrompt.style.display = 'none';
       }
-    });
-  
-    startButton.addEventListener('click', startQuiz);
+    }
+  });
+
+  continueButton.addEventListener('click', () => {
+    const storagedQuestionIndex = localStorage.getItem('questionIndex');
+    console.log('storagedQuestionIndex', storagedQuestionIndex);
+    if (storagedQuestionIndex) {
+      initQuestionPage(Number(storagedQuestionIndex));
+    } else {
+      initQuestionPage();
+    }
+  });
+
+  startButton.addEventListener('click', startQuiz);
 };
+
 const startQuiz = () => {
   console.log('Starting the game...');
   initQuestionPage();
 };
-
-
-
--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,7 @@
 import { quizData } from './data.js';
 import { initQuestionPage } from './pages/questionPage.js';
-import{initEndingPage} from './pages/endingPage.js'
-
+import { initEndingPage } from './pages/endingPage.js';
+import { initWelcomePage } from './pages/welcomePage.js';
 
 export const processAnswer = (isCorrect) => {
   if (isCorrect) {
@@ -22,5 +22,8 @@ const endQuiz = () => {
   export const resetQuiz = () => {
     quizData.currentQuestionIndex = 0;
     quizData.score = 0;
-    initQuestionPage();
+    localStorage.clear('score');
+    localStorage.clear('playerName');
+    localStorage.clear('questionIndex');
+    initWelcomePage();
   };
