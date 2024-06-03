f6fbcf81291f3b47c4c6c256f4be082217c501f5
mustafa durmus <mdurmusnl@gmail.com>
2024-06-03 14:51:06 +0200

    eslint

--- src/app.js
+++ src/app.js
@@ -2,13 +2,9 @@ import { ANSWERS_LIST_ID, NEW_GAME_BUTTON_ID, USER_INTERFACE_ID } from './consta
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
-
-
-
 const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
-  quizData.score=0;
-  initWelcomePage();
-
+    quizData.currentQuestionIndex = 0;
+    quizData.score = 0;
+    initWelcomePage();
 };
 window.addEventListener('load', loadApp);
--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,9 +1,4 @@
-import {
-  ANSWERS_LIST_ID,
-  NEXT_QUESTION_BUTTON_ID,
-  USER_INTERFACE_ID,
-  SKIP_QUESTION_BUTTON_ID,
-} from '../constants.js';
+import { ANSWERS_LIST_ID, NEXT_QUESTION_BUTTON_ID, USER_INTERFACE_ID, SKIP_QUESTION_BUTTON_ID } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
@@ -13,143 +8,139 @@ import { initEndingPage } from './endingPage.js';
 let questionAnswered = false;
 export let score = 0;
 
-export const initQuestionPage = (index) => {
-  const storagedScore = localStorage.getItem('score');
-  score = storagedScore ? Number(storagedScore) : 0;
+export const initQuestionPage = index => {
+    const storagedScore = localStorage.getItem('score');
+    score = storagedScore ? Number(storagedScore) : 0;
 
-  if (index !== undefined) {
-    quizData.currentQuestionIndex = index;
-  }
+    if (index !== undefined) {
+        quizData.currentQuestionIndex = index;
+    }
 
-  if (quizData.currentQuestionIndex >= quizData.questions.length) {
-    initEndingPage();
-    return;
-  }
+    if (quizData.currentQuestionIndex >= quizData.questions.length) {
+        initEndingPage();
+        return;
+    }
 
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.innerHTML = '';
 
-  const scoreContainer = document.createElement('div');
-  scoreContainer.classList.add('score-container');
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('score-container');
 
-  const scoreElement = document.createElement('div');
-  scoreElement.classList.add('score');
-  scoreElement.textContent = `Your Score is: ${score}`;
-  scoreContainer.appendChild(scoreElement);
-  userInterface.appendChild(scoreContainer);
+    const scoreElement = document.createElement('div');
+    scoreElement.classList.add('score');
+    scoreElement.textContent = `Your Score is: ${score}`;
+    scoreContainer.appendChild(scoreElement);
+    userInterface.appendChild(scoreContainer);
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
 
-  const questionElement = createQuestionElement(currentQuestion.text);
-  userInterface.appendChild(questionElement);
+    const questionElement = createQuestionElement(currentQuestion.text);
+    userInterface.appendChild(questionElement);
 
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
-  answersListElement.innerHTML = ''; // Clear previous answers
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    answersListElement.innerHTML = ''; // Clear previous answers
 
-  for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    const answerElement = createAnswerElement(key, answerText);
-    answerElement.addEventListener('click', () => selectAnswer(key, answerElement));
-    answersListElement.appendChild(answerElement);
-  }
+    for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
+        const answerElement = createAnswerElement(key, answerText);
+        answerElement.addEventListener('click', () => selectAnswer(key, answerElement));
+        answersListElement.appendChild(answerElement);
+    }
 
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+    document.getElementById(NEXT_QUESTION_BUTTON_ID).addEventListener('click', nextQuestion);
 
-  document
-    .getElementById(SKIP_QUESTION_BUTTON_ID)
-    .addEventListener('click', skipQuestion);
+    document.getElementById(SKIP_QUESTION_BUTTON_ID).addEventListener('click', skipQuestion);
 
-  questionAnswered = false;
-  nextButtonSwitcher();
+    questionAnswered = false;
+    nextButtonSwitcher();
 };
 
 const skipQuestion = () => {
-  if (questionAnswered) return;
+    if (questionAnswered) return;
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
-  const allAnswers = answersListElement.getElementsByTagName('li');
-  for (let i = 0; i < allAnswers.length; i++) {
-    const answerKey = allAnswers[i].getAttribute('data-key');
-    if (answerKey === currentQuestion.correct) {
-      allAnswers[i].classList.add('skipped-correct');
-      break;
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    const allAnswers = answersListElement.getElementsByTagName('li');
+    for (let i = 0; i < allAnswers.length; i++) {
+        const answerKey = allAnswers[i].getAttribute('data-key');
+        if (answerKey === currentQuestion.correct) {
+            allAnswers[i].classList.add('skipped-correct');
+            break;
+        }
     }
-  }
 
-  questionAnswered = true;
-  nextButtonSwitcher();
-  skipButtonSwitcher();
+    questionAnswered = true;
+    nextButtonSwitcher();
+    skipButtonSwitcher();
 };
 
 const selectAnswer = (key, answerElement) => {
-  if (questionAnswered) return;
+    if (questionAnswered) return;
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
-  const allAnswers = answersListElement.getElementsByTagName('li');
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    const allAnswers = answersListElement.getElementsByTagName('li');
 
-  if (key === currentQuestion.correct) {
-    answerElement.style.backgroundColor = 'green';
-    score++;
-    displayHappyCat();
-  } else {
-    answerElement.style.backgroundColor = 'red';
-    displayUnhappyCat();
-    setTimeout(() => {
-      for (let i = 0; i < allAnswers.length; i++) {
-        const answerKey = allAnswers[i].getAttribute('data-key');
-        if (answerKey === currentQuestion.correct) {
-          allAnswers[i].style.backgroundColor = 'green';
-          break;
-        }
-      }
-    }, 500);
-  }
+    if (key === currentQuestion.correct) {
+        answerElement.style.backgroundColor = 'green';
+        score++;
+        displayHappyCat();
+    } else {
+        answerElement.style.backgroundColor = 'red';
+        displayUnhappyCat();
+        setTimeout(() => {
+            for (let i = 0; i < allAnswers.length; i++) {
+                const answerKey = allAnswers[i].getAttribute('data-key');
+                if (answerKey === currentQuestion.correct) {
+                    allAnswers[i].style.backgroundColor = 'green';
+                    break;
+                }
+            }
+        }, 500);
+    }
 
-  questionAnswered = true;
-  skipButtonSwitcher();
-  nextButtonSwitcher();
+    questionAnswered = true;
+    skipButtonSwitcher();
+    nextButtonSwitcher();
 };
 
 const nextQuestion = () => {
-  quizData.currentQuestionIndex++;
-  localStorage.setItem('questionIndex', quizData.currentQuestionIndex);
-  localStorage.setItem('score', score);
+    quizData.currentQuestionIndex++;
+    localStorage.setItem('questionIndex', quizData.currentQuestionIndex);
+    localStorage.setItem('score', score);
 
-  initQuestionPage(quizData.currentQuestionIndex);
+    initQuestionPage(quizData.currentQuestionIndex);
 };
 
 const nextButtonSwitcher = () => {
-  document.getElementById(NEXT_QUESTION_BUTTON_ID).disabled = !questionAnswered;
+    document.getElementById(NEXT_QUESTION_BUTTON_ID).disabled = !questionAnswered;
 };
 
 const skipButtonSwitcher = () => {
-  const skipButton = document.getElementById(SKIP_QUESTION_BUTTON_ID);
-  skipButton.disabled = questionAnswered;
+    const skipButton = document.getElementById(SKIP_QUESTION_BUTTON_ID);
+    skipButton.disabled = questionAnswered;
 };
 
 const displayHappyCat = () => {
-  const happyCatImg = document.createElement('img');
-  happyCatImg.src = 'https://media.tenor.com/cS2O4bhrjLkAAAAM/happy-pleased.gif';
-  happyCatImg.alt = 'Happy Cat';
-  happyCatImg.classList.add('show-happy-cat');
-  document.body.appendChild(happyCatImg);
+    const happyCatImg = document.createElement('img');
+    happyCatImg.src = 'https://media.tenor.com/cS2O4bhrjLkAAAAM/happy-pleased.gif';
+    happyCatImg.alt = 'Happy Cat';
+    happyCatImg.classList.add('show-happy-cat');
+    document.body.appendChild(happyCatImg);
 
-  setTimeout(() => {
-    happyCatImg.remove();
-  }, 2000);
+    setTimeout(() => {
+        happyCatImg.remove();
+    }, 2000);
 };
 
 const displayUnhappyCat = () => {
-  const unhappyCatImg = document.createElement('img');
-  unhappyCatImg.src = 'https://media.tenor.com/xCO75gIMoCoAAAAM/catsad-sad.gif';
-  unhappyCatImg.alt = 'Unhappy Cat';
-  unhappyCatImg.classList.add('show-unhappy-cat');
-  document.body.appendChild(unhappyCatImg);
+    const unhappyCatImg = document.createElement('img');
+    unhappyCatImg.src = 'https://media.tenor.com/xCO75gIMoCoAAAAM/catsad-sad.gif';
+    unhappyCatImg.alt = 'Unhappy Cat';
+    unhappyCatImg.classList.add('show-unhappy-cat');
+    document.body.appendChild(unhappyCatImg);
 
-  setTimeout(() => {
-    unhappyCatImg.remove();
-  }, 2000);
+    setTimeout(() => {
+        unhappyCatImg.remove();
+    }, 2000);
 };
