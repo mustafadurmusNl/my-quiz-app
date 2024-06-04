333573529aaddf0f8ceebd797ea3f3dc01f9f43e
mustafa durmus <mdurmusnl@gmail.com>
2024-06-04 13:52:35 +0200

    welcomePage updated in modular functional way

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
     quizData.currentQuestionIndex = 0;
-    quizData.score = 0;
     initWelcomePage();
 };
 window.addEventListener('load', loadApp);
--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,117 +1,124 @@
-import {
-  USER_INTERFACE_ID,
-  START_QUIZ_BUTTON_ID,
-  CONFIRM_BUTTON_ID,
-  INPUT_NAME_ID,
-  INPUT_P_TAG_ID,
-  WELCOME_MESSAGE_ID,
-  CONTINUE_BUTTON_ID,
-} from '../constants.js';
+import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID, CONFIRM_BUTTON_ID, INPUT_NAME_ID, INPUT_P_TAG_ID, WELCOME_MESSAGE_ID, CONTINUE_BUTTON_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 
 export const initWelcomePage = () => {
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.innerHTML = '';
 
-  const welcomeElement = createWelcomeElement();
-  userInterface.appendChild(welcomeElement);
+    const welcomeElement = createWelcomeElement();
+    userInterface.appendChild(welcomeElement);
 
-  document
-    .getElementById(START_QUIZ_BUTTON_ID)
-    .addEventListener('click', startQuiz);
-  const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
-  const continueButton = document.getElementById(CONTINUE_BUTTON_ID);
-  const startButton = document.getElementById(START_QUIZ_BUTTON_ID);
-  const nameInput = document.getElementById(INPUT_NAME_ID);
-  const namePrompt = document.getElementById(INPUT_P_TAG_ID);
-  const welcomeMessage = document.getElementById(WELCOME_MESSAGE_ID);
+    document.getElementById(START_QUIZ_BUTTON_ID).addEventListener('click', startQuiz);
+    const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
+    const continueButton = document.getElementById(CONTINUE_BUTTON_ID);
+    const startButton = document.getElementById(START_QUIZ_BUTTON_ID);
+    const nameInput = document.getElementById(INPUT_NAME_ID);
+    const namePrompt = document.getElementById(INPUT_P_TAG_ID);
 
-  function updateConfirmButtonState() {
-    nameInput.value = nameInput.value.trimStart();
-    const name = nameInput.value;
-    if (name) {
-      confirmButton.classList.remove('button-disabled');
-      confirmButton.classList.add('button-enabled');
-      confirmButton.disabled = false;
-      confirmButton.style.backgroundColor = 'rgb(221, 113, 162)';
-      confirmButton.style.cursor = 'pointer';
-    } else {
-      confirmButton.classList.add('button-disabled');
-      confirmButton.classList.remove('button-enabled');
-      confirmButton.disabled = true;
-      confirmButton.style.backgroundColor = 'rgb(232, 232, 232)';
-      confirmButton.style.cursor = 'not-allowed';
-    }
-  }
-
-  function updateContinueButtonState() {
-    const storagedPlayerName = localStorage.getItem('playerName');
-    if (storagedPlayerName) {
-      continueButton.classList.remove('button-disabled');
-      continueButton.classList.add('button-enabled');
-      continueButton.disabled = false;
-      continueButton.style.backgroundColor = 'rgb(221, 113, 162)';
-      continueButton.style.cursor = 'pointer';
-    } else {
-      continueButton.classList.add('button-disabled');
-      continueButton.classList.remove('button-enabled');
-      continueButton.disabled = true;
-      continueButton.style.backgroundColor = 'rgb(232, 232, 232)';
-      continueButton.style.cursor = 'not-allowed';
-    }
-  }
-
-  updateConfirmButtonState();
-  nameInput.addEventListener('input', updateConfirmButtonState);
-
-  updateContinueButtonState();
-
-  confirmButton.addEventListener('click', () => {
-    const playerName = nameInput.value.trim();
-    const storagedPlayerName = localStorage.getItem('playerName');
-    if (playerName) {
-      if (playerName === storagedPlayerName) {
-        const storagedQuestionIndex = localStorage.getItem('questionIndex');
-        console.log('storagedQuestionIndex', storagedQuestionIndex);
-        if (storagedQuestionIndex) {
-          initQuestionPage(Number(storagedQuestionIndex));
+    function updateConfirmButtonState() {
+        nameInput.value = nameInput.value.trimStart();
+        const name = nameInput.value;
+        if (name) {
+            enableConfirmButton();
         } else {
-          initQuestionPage();
+            disableConfirmButton();
         }
-      } else {
-        localStorage.setItem('score', 0);
-        localStorage.setItem('playerName', playerName);
-        welcomeMessage.innerText = `Welcome , ${playerName}`;
-        welcomeMessage.classList.add('welcome-effect');
-        welcomeMessage.style.padding = '20px';
-        setTimeout(() => {
-          welcomeMessage.style.opacity = 1;
-          welcomeMessage.classList.add('green-text');
-        }, 100);
-        confirmButton.style.display = 'none';
-        continueButton.style.display = 'none';
-        startButton.style.display = 'block';
-        nameInput.style.display = 'none';
-        namePrompt.style.display = 'none';
-      }
     }
-  });
 
-  continueButton.addEventListener('click', () => {
-    const storagedQuestionIndex = localStorage.getItem('questionIndex');
-    console.log('storagedQuestionIndex', storagedQuestionIndex);
-    if (storagedQuestionIndex) {
-      initQuestionPage(Number(storagedQuestionIndex));
-    } else {
-      initQuestionPage();
+    function updateContinueButtonState() {
+        const storagedPlayerName = localStorage.getItem('playerName');
+        if (storagedPlayerName) {
+            enableContinueButton();
+        } else {
+            disableContinueButton();
+        }
     }
-  });
 
-  startButton.addEventListener('click', startQuiz);
+    updateConfirmButtonState();
+    nameInput.addEventListener('input', updateConfirmButtonState);
+
+    updateContinueButtonState();
+
+    confirmButton.addEventListener('click', () => {
+        const playerName = nameInput.value.trim();
+        const storagedPlayerName = localStorage.getItem('playerName');
+        if (playerName) {
+            if (playerName === storagedPlayerName) {
+                const storagedQuestionIndex = localStorage.getItem('questionIndex');
+                console.log('storagedQuestionIndex', storagedQuestionIndex);
+                if (storagedQuestionIndex) {
+                    initQuestionPage(Number(storagedQuestionIndex));
+                } else {
+                    initQuestionPage();
+                }
+            } else {
+                localStorage.setItem('score', 0);
+                localStorage.setItem('playerName', playerName);
+                createWelcomeMessage(playerName);
+                confirmButton.style.display = 'none';
+                continueButton.style.display = 'none';
+                startButton.style.display = 'block';
+                nameInput.style.display = 'none';
+                namePrompt.style.display = 'none';
+            }
+        }
+    });
+
+    continueButton.addEventListener('click', () => {
+        const storagedQuestionIndex = localStorage.getItem('questionIndex');
+        if (storagedQuestionIndex) {
+            initQuestionPage(Number(storagedQuestionIndex));
+        } else {
+            initQuestionPage();
+        }
+    });
+
+    startButton.addEventListener('click', startQuiz);
 };
 
 const startQuiz = () => {
-  console.log('Starting the game...');
-  initQuestionPage();
+    initQuestionPage();
+};
+const enableConfirmButton = () => {
+    const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
+    confirmButton.classList.remove('button-disabled');
+    confirmButton.classList.add('button-enabled');
+    confirmButton.disabled = false;
+    confirmButton.style.backgroundColor = 'rgb(221, 113, 162)';
+    confirmButton.style.cursor = 'pointer';
+};
+const disableConfirmButton = () => {
+    const confirmButton = document.getElementById(CONFIRM_BUTTON_ID);
+    confirmButton.classList.remove('button-disabled');
+    confirmButton.classList.add('button-enabled');
+    confirmButton.disabled = true;
+    confirmButton.style.backgroundColor = 'rgb(221, 113, 162)';
+    confirmButton.style.cursor = 'pointer';
+};
+const enableContinueButton = () => {
+    const continueButton = document.getElementById(CONTINUE_BUTTON_ID);
+    continueButton.classList.remove('button-disabled');
+    continueButton.classList.add('button-enabled');
+    continueButton.disabled = false;
+    continueButton.style.backgroundColor = 'rgb(221, 113, 162)';
+    continueButton.style.cursor = 'pointer';
+};
+const disableContinueButton = () => {
+    const continueButton = document.getElementById(CONTINUE_BUTTON_ID);
+    continueButton.classList.remove('button-disabled');
+    continueButton.classList.add('button-enabled');
+    continueButton.disabled = true;
+    continueButton.style.backgroundColor = 'rgb(221, 113, 162)';
+    continueButton.style.cursor = 'pointer';
+};
+const createWelcomeMessage = playerName => {
+    const welcomeMessage = document.getElementById(WELCOME_MESSAGE_ID);
+    welcomeMessage.innerText = `Welcome , ${playerName}`;
+    welcomeMessage.classList.add('welcome-effect');
+    welcomeMessage.style.padding = '20px';
+    setTimeout(() => {
+        welcomeMessage.style.opacity = 1;
+        welcomeMessage.classList.add('green-text');
+    }, 2000);
 };
